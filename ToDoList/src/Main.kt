6d012b794e6ff6a48b6973d3f96a90b870044cae
Ujjwal Chauhan <ujjwalchauhan960@gmail.com>
2026-09-06 00:57:09 +0530

    edited todolist

--- ToDoList/src/Main.kt
+++ ToDoList/src/Main.kt
@@ -8,29 +8,42 @@ fun main() {
     var taskList = mutableListOf<String>()
     var option = readLine()!!.toInt()
     var isRunning = true
-
+    var completeTasks=mutableListOf<String>()
     while (isRunning) {
         if (option == 1) {
             println("Enter task")
             var task = readLine()!!.toString()
             taskList.add(task)
             println("task added")
+            println("What do you want to do next?")
             option = readLine()!!.toInt()
+
         } else if (option == 2) {
             println("task list =$taskList")
+            println("What do you want to do next?")
             option = readLine()!!.toInt()
+
         } else if (option == 3) {
             println("which task is complete")
             var taskNo = readLine()!!.toInt()
             taskList.removeAt(taskNo - 1)
-            println("task removed")
+            completeTasks.add(taskList.removeAt(taskNo - 1))
+            println("Task completed :$completeTasks")
+            println("What to do next:$taskList")
+            println("What do you want to do next?")
+
             option = readLine()!!.toInt()
+
         } else if (option == 4) {
             println("Which task to del")
             var taskNo = readLine()!!.toInt()
             taskList.removeAt(taskNo - 1)
+            println("We are deleting [${taskList.removeAt(taskNo - 1)}]")
             println("task deleted")
+            println("Remaning tasks $taskList")
+            println("What do you want to do next?")
             option = readLine()!!.toInt()
+
         } else if (option == 5) {
             isRunning = false
             println("We r out")
