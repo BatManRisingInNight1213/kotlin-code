6ea2f8fe3aefa27c6ee6d9fc435e2f62472c4de5
Ujjwal Chauhan <ujjwalchauhan960@gmail.com>
2026-09-09 15:41:37 +0530

    updationg ToDoList

--- ToDoList/src/Main.kt
+++ ToDoList/src/Main.kt
@@ -4,59 +4,61 @@ fun main() {
     println("Enter 3 to Complete task")
     println("Enter 4 to Delete task")
     println("Enter 5 to Exit")
-
     var taskList = mutableListOf<String>()
-    var option = readLine()!!.toInt()
+    var option = readLine()!!.toIntOrNull()
     var isRunning = true
-    var completeTasks=mutableListOf<String>()
-    if (option is Int){
-        while (isRunning) {
-            if (option == 1) {
-                println("Enter task")
-                var task = readLine()!!.toString()
-                if (task!=null){
-                    taskList.add(task)
-                }
-
+    var completeTasks = mutableListOf<String>()
+    while (isRunning) {
+        if (option == 1) {
+            println("Enter task")
+            var task = readLine()!!.toString()
+            if (task != null) {
+                taskList.add(task)
                 println("task added")
-                println("What do you want to do next?")
-                option = readLine()!!.toInt()
-
-            } else if (option == 2) {
-                println("task list =$taskList")
-                println("What do you want to do next?")
-                option = readLine()!!.toInt()
-
-            } else if (option == 3) {
-                println("which task is complete")
-                var taskNo = readLine()!!.toInt()
-                taskList.removeAt(taskNo - 1)
-                completeTasks.add(taskList.removeAt(taskNo - 1))
-                println("Task completed :$completeTasks")
-                println("What to do next:$taskList")
-                println("What do you want to do next?")
-
-                option = readLine()!!.toInt()
-
-            } else if (option == 4) {
-                println("Which task to del")
-                var taskNo = readLine()!!.toInt()
-                taskList.removeAt(taskNo - 1)
-                println("We are deleting [${taskList.removeAt(taskNo - 1)}]")
-                println("task deleted")
-                println("Remaning tasks $taskList")
-                println("What do you want to do next?")
-                option = readLine()!!.toInt()
-
-            } else if (option == 5) {
-                isRunning = false
-                println("We r out")
-            } else {
-                println("Invalid input")
-                option = readLine()!!.toInt()
-                
             }
+            println("What do you want to do next?")
+            option = readLine()!!.toIntOrNull()
+
+        } else if (option == 2) {
+            println("task list =$taskList")
+            println("What do you want to do next?")
+            option = readLine()!!.toIntOrNull()
+
+        } else if (option == 3) {
+            println("which task is complete")
+            var taskNo = readLine()!!.toInt()
+           // taskList.removeAt(taskNo - 1)
+            if (taskList != null) {completeTasks.add(taskList.removeAt(taskNo - 1))}
+
+            println("Task completed :$completeTasks")
+            println("What to do next:$taskList")
+            println("What do you want to do next?")
+            option = readLine()!!.toIntOrNull()
+
+        } else if (option == 4) {
+            println("Which task to del")
+            var taskNo = readLine()!!.toInt()
+            if (taskList != null) {println("We are deleting [${taskList.removeAt(taskNo - 1)}]")}
+            //taskList.removeAt(taskNo - 1)
+
+            println("task deleted")
+            println("Remaning tasks $taskList")
+            println("What do you want to do next?")
+            option = readLine()!!.toIntOrNull()
+
+        } else if (option == 5) {
+            isRunning = false
+            println("We r out")
+        } else {
+            println("Invalid input")
+            option = readLine()!!.toIntOrNull()
         }
     }
+}
 
+fun checkType(option: Any):Boolean {
+    if (option is Int) {
+        return true
+    } else println("Enter valid number")
+    return false
 }
