9f39b6ce83359fcb380d04e57db48628ef796a95
Ujjwal Chauhan <ujjwalchauhan960@gmail.com>
2026-09-09 15:10:28 +0530

    updated todolist

--- ToDoList/src/Main.kt
+++ ToDoList/src/Main.kt
@@ -9,44 +9,54 @@ fun main() {
     var option = readLine()!!.toInt()
     var isRunning = true
     var completeTasks=mutableListOf<String>()
-    while (isRunning) {
-        if (option == 1) {
-            println("Enter task")
-            var task = readLine()!!.toString()
-            taskList.add(task)
-            println("task added")
-            println("What do you want to do next?")
-            option = readLine()!!.toInt()
+    if (option is Int){
+        while (isRunning) {
+            if (option == 1) {
+                println("Enter task")
+                var task = readLine()!!.toString()
+                if (task!=null){
+                    taskList.add(task)
+                }
 
-        } else if (option == 2) {
-            println("task list =$taskList")
-            println("What do you want to do next?")
-            option = readLine()!!.toInt()
+                println("task added")
+                println("What do you want to do next?")
+                option = readLine()!!.toInt()
 
-        } else if (option == 3) {
-            println("which task is complete")
-            var taskNo = readLine()!!.toInt()
-            taskList.removeAt(taskNo - 1)
-            completeTasks.add(taskList.removeAt(taskNo - 1))
-            println("Task completed :$completeTasks")
-            println("What to do next:$taskList")
-            println("What do you want to do next?")
+            } else if (option == 2) {
+                println("task list =$taskList")
+                println("What do you want to do next?")
+                option = readLine()!!.toInt()
 
-            option = readLine()!!.toInt()
+            } else if (option == 3) {
+                println("which task is complete")
+                var taskNo = readLine()!!.toInt()
+                taskList.removeAt(taskNo - 1)
+                completeTasks.add(taskList.removeAt(taskNo - 1))
+                println("Task completed :$completeTasks")
+                println("What to do next:$taskList")
+                println("What do you want to do next?")
 
-        } else if (option == 4) {
-            println("Which task to del")
-            var taskNo = readLine()!!.toInt()
-            taskList.removeAt(taskNo - 1)
-            println("We are deleting [${taskList.removeAt(taskNo - 1)}]")
-            println("task deleted")
-            println("Remaning tasks $taskList")
-            println("What do you want to do next?")
-            option = readLine()!!.toInt()
+                option = readLine()!!.toInt()
 
-        } else if (option == 5) {
-            isRunning = false
-            println("We r out")
+            } else if (option == 4) {
+                println("Which task to del")
+                var taskNo = readLine()!!.toInt()
+                taskList.removeAt(taskNo - 1)
+                println("We are deleting [${taskList.removeAt(taskNo - 1)}]")
+                println("task deleted")
+                println("Remaning tasks $taskList")
+                println("What do you want to do next?")
+                option = readLine()!!.toInt()
+
+            } else if (option == 5) {
+                isRunning = false
+                println("We r out")
+            } else {
+                println("Invalid input")
+                option = readLine()!!.toInt()
+                
+            }
         }
     }
+
 }
