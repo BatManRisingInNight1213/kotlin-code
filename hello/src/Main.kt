58ec2140da569a13749e0b792586f146abfa22b8
Ujjwal Chauhan <ujjwalchauhan960@gmail.com>
2026-09-13 22:15:03 +0530

    completed challenge question

--- hello/src/Main.kt
+++ hello/src/Main.kt
@@ -1,6 +1,7 @@
 import kotlin.random.Random
 
 fun main() {
+    challenege()
     println("Enter a no. b/w 1 - 100")
     var noOfGuess = 0
     val randomNo = Random.nextInt(0, 100)
@@ -22,7 +23,18 @@ fun main() {
             println("type smaller no")
             answer = readLine()!!.toInt()
         }
+
+    }}
+
+fun challenege(){
+    var count=0
+    var last=readLine()!!.toInt()
+    for (i in 0..last){
+        if (i%2==0){
+            count++
+            println(i)
+
+        }
     }
-    var playAgain =false
-    if (playAgain) {}
+    println(count)
 }
