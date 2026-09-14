cda663090eca68e5150d160630c4098f437140e7
Ujjwal Chauhan <ujjwalchauhan960@gmail.com>
2026-09-14 23:21:06 +0530

    added challenge2

---
+++ challenge2/src/Main.kt
@@ -0,0 +1,25 @@
+fun main()
+{
+    var a: Array<Int> = arrayOf(1,4,4,344,5,36)
+ println(largest(a))
+}
+fun largest(a: Array<Int>):Int {
+    var max =0
+    var temp=0
+    for (i in a){
+        temp=i
+        max = maxOf(max,temp)
+    }
+    println("largest number is:")
+    return max
+}
+fun smallest(a: Array<Int>):Int {
+    var min =0
+    var temp=0
+    for (i in a){
+        temp=i
+        min = minOf(min,temp)
+    }
+    println("smallest number is:")
+    return min
+}
